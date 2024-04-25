e22f70522805376c339ce1c7f3252a6a1c51a905
FCM <carrascomerchan@msn.com>
2024-04-25 13:21:53 +0200

    commit

--- first-project/part1/src/App.jsx
+++ first-project/part1/src/App.jsx
@@ -1,3 +1,40 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+        <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+        <h2>{props.part}</h2>
+        <p>Number of exercises:{props.exercise}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+      <div>
+        <Part part={props.part1} exercise={props.exercises1} />
+        <Part part={props.part2} exercise={props.exercises2} />
+        <Part part={props.part3} exercise={props.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+        <p>Total of exercises: {props.one + props.two + props.three}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // const-definitions
   const course = 'Half Stack application development'
@@ -11,36 +48,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content name={part1} exercises={exercises1} /> 
-      <Content name={part2} exercises={exercises2}/> 
-      <Content name={part3} exercises={exercises3}/> 
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total one = {exercises1} two = {exercises2} three = {exercises3} />
-
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <div>
-        <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-        <h2>{props.name}</h2>
-        <p>Number of exercises:{props.exercises}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-        <p>Total of exercises: {props.one + props.two + props.three}</p>
     </div>
   )
 }
