b40bb600f0aaddf11ad0db919cdc1bf5417dafce
FCM <carrascomerchan@msn.com>
2024-04-26 11:02:51 +0200

    commit

--- first-project/part1/src/App.jsx
+++ first-project/part1/src/App.jsx
@@ -1,57 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Header = (props) => {
-  return (
-    <div>
-        <h1>{props.course}</h1>
-    </div>
-  )
-}
+const Header = ({ text }) => <h1>{text}</h1>;
 
-const Part = (props) => {
-  return (
-    <div>
-        <h2>{props.part}</h2>
-        <p>Number of exercises:{props.exercise}</p>
-    </div>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Content = (props) => {
+const Statistic = ({ text, value }) => {
   return (
-      <div>
-        <Part part={props.part1} exercise={props.exercises1} />
-        <Part part={props.part2} exercise={props.exercises2} />
-        <Part part={props.part3} exercise={props.exercises3} />
-    </div>
-  )
-}
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
 
-const Total = (props) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  if (total === 0) {
+    return <div>No feedback given</div>;
+  }
+
+  const average = () => (good - bad) / total;
+
+  const positive = () => (good / total) * 100 + " %";
+
   return (
-    <div>
-        <p>Total of exercises: {props.one + props.two + props.three}</p>
-    </div>
-  )
-}
+    <table>
+      <tbody>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={total} />
+        <Statistic text="average" value={average()} />
+        <Statistic text="positive" value={positive()} />
+      </tbody>
+    </table>
+  );
+};
 
 const App = () => {
-  // const-definitions
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total one = {exercises1} two = {exercises2} three = {exercises3} />
+      <Header text={"give feedback"} />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+      <Header text={"statistics"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
 export default App
