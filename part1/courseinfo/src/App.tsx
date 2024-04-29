d2ffa11cde5368d68884cf42af538bf203fc6739
FCM <carrascomerchan@msn.com>
2024-04-29 11:49:32 +0200

    commit

---
+++ part1/courseinfo/src/App.tsx
@@ -0,0 +1,34 @@
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <h2>{course.parts[0].name}</h2>
+      <p>Exercises: {course.parts[0].exercises}</p>
+      <h2>{course.parts[1].name}</h2>
+      <p>Exercises: {course.parts[1].exercises}</p>
+      <h2>{course.parts[2].name}</h2>
+      <p>Exercises: {course.parts[1].exercises}</p>
+    </div>
+  )
+}
+
+export default App
